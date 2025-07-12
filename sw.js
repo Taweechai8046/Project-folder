b238e2d04bcba854627e28bd96ad6b2545b73436
Taweechai Mawing <dairmawingfongbeer@gmail.com>
2025-07-12 23:45:16 +0700

    update

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'gold-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+  // เพิ่มไฟล์อื่น ๆ ที่คุณใช้
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
